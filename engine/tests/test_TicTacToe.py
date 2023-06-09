1963cea743dc80f21d7bbe431f6bc2538b42b333
SilverStardust <Yuristeelf@gmail.com>
2023-06-09 14:16:43 +0200

    test: :white_check_mark: Test TicTacToe.py => set_target
    adding test for the function set_target in the TicTacToe.py file

--- engine/tests/test_TicTacToe.py
+++ engine/tests/test_TicTacToe.py
@@ -17,7 +17,8 @@ from TicTacToe import TicTacToe
 # <========== Tests ===========>
 
 class Testing(unittest.TestCase):
-    def test_constructor(self):
+
+    def test_constructor(self: Testing):
         self.assertEqual(TicTacToe().current_player, 0)
         self.assertEqual(TicTacToe(1).current_player, 1)
 
@@ -27,6 +28,27 @@ class Testing(unittest.TestCase):
         with self.assertRaises(Exception) as context: TicTacToe(-1)
         self.assertTrue("the first player must be 0 (player 1) or 1 (player 2)" in str(context.exception))
 
+        with self.assertRaises(Exception) as context: TicTacToe(-2)
+
+    def test_set_target(self: Testing):
+        t: TicTacToe = TicTacToe()
+
+        self.assertTrue(t.set_target(1))
+        self.assertEqual(t.__str__(), "['X' ' ' ' ']\n[' ' ' ' ' ']\n[' ' ' ' ' ']\n")
+        self.assertEqual(t.current_player, 1)
+
+        self.assertTrue(t.set_target(2))
+        self.assertEqual(t.__str__(), "['X' 'O' ' ']\n[' ' ' ' ' ']\n[' ' ' ' ' ']\n")
+        self.assertEqual(t.current_player, 0)
+
+        self.assertFalse(t.set_target(1))
+        self.assertEqual(t.__str__(), "['X' 'O' ' ']\n[' ' ' ' ' ']\n[' ' ' ' ' ']\n")
+        self.assertEqual(t.current_player, 0)
+
+        t: TicTacToe = TicTacToe()
+        for i in range(1,10): t.set_target(i)
+        self.assertEqual(t.__str__(), "['X' 'O' 'X']\n['O' 'X' 'O']\n['X' 'O' 'X']\n")
+        self.assertEqual(t.current_player, 0)
 
 # <========== Main ==========>
 if __name__ == '__main__':
